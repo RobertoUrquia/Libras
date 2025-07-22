e94827413086055e9f0d4897de8e2f77f9ed48ec
RobertoUrquia <99776722+RobertoUrquia@users.noreply.github.com>
2025-07-22 15:53:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,9 @@ def predecir():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+@app.route('/')
+def index():
+    return 'API para convertir libras a kilogramos. Usa la ruta /convertir para enviar datos.'
+
